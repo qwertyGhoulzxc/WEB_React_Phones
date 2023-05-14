ffa1e1cfc4b73394d46c57f5ec49c141d9a1500b
qwertyGhoulzxc <pos2.ebasha@gmail.com>
2023-05-14 15:01:03 +0300

    add routes

---
+++ app/components/PhoneDescription/PhoneDescription.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import Layout from '../Layout/Layout'
+import PhoneVisual from './PhoneVisual/PhoneVisual'
+
+const PhoneDescription: FC = () => {
+  return <Layout title='PhoneDescription'>
+    
+    <PhoneVisual/>
+    
+
+    
+    
+    </Layout>
+}
+
+export default PhoneDescription
---
+++ pages/phonedescription.tsx
@@ -0,0 +1,8 @@
+import PhoneDescription from '@/app/components/PhoneDescription/PhoneDescription'
+import { FC } from 'react'
+
+const phonedescription: FC = () => {
+  return <PhoneDescription/>
+}
+
+export default phonedescription
