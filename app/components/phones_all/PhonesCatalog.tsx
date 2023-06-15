fd0e60b77c93a7e7d0d9b4e7e39b5c754df87012
qwertyGhoulzxc <pos2.ebasha@gmail.com>
2023-06-15 23:44:46 +0300

    add pagination

--- app/components/phones_all/PhonesCatalog.tsx
+++ app/components/phones_all/PhonesCatalog.tsx
@@ -4,22 +4,13 @@ import HeaderPart from './header/HeaderPart'
 import GoodsList from './GoodsList/GoodsList'
 import FilterWindow from './FilterWindow/FilterWindow'
 import styles from './PhonesCatatlog.module.scss'
-import {useRouter} from 'next/router'
 import Pagination from "@/app/components/phones_all/pagination/Pgagination";
 
 
 const PhonesCatalog: FC = () => {
-    const {query: pageQuery, push} = useRouter()
-    const handleClick = () => {
-        console.log(pageQuery);
-        push({
-            pathname: '/goods/phones',
-            query: {page: '2'},
-        });
-    };
+
 
     return <Layout title='Iphones'>
-        <button onClick={handleClick}>query</button>
         <HeaderPart/>
         <div className={styles.pageContainer}>
             <GoodsList/>
--- app/components/phones_all/pagination/Pgagination.tsx
+++ app/components/phones_all/pagination/Pgagination.tsx
@@ -1,12 +1,42 @@
 import {FC} from 'react'
 import {useAppSelector} from "@/app/hooks/redux";
+import styles from './pagination.module.scss'
+import {BsArrowLeftCircle, BsArrowRightCircle} from 'react-icons/bs'
+import {useRouter} from "next/router";
 
 const Pagination: FC = () => {
-    const {} = useAppSelector(state => state.phonesDataReducer)
+    const {page, limit, total,} = useAppSelector(state => state.phonesDataReducer)
+    const totalPages = Math.ceil(total / limit)
+    const pagesUi = []
+    for (let i = 1; i <= totalPages; i++) {
+        pagesUi.push(i)
+    }
 
-    return <div>
+    const {push} = useRouter()
+    const handleLeftArrow = () => {
+        push(`/goods/phones?page=${page - 1}`)
+    }
 
-        adasd
+    const handleRightArrow = () => {
+        push(`/goods/phones?page=${page + 1}`)
+    }
+
+    const handleRedirect = (pageVal) => {
+        if (page !== page)
+            push(`/goods/phones?page=${pageVal}`)
+    }
+
+
+    return <div className={styles.container}>
+        <div className={styles.pagesContainer}>
+            {page !== 1 && <BsArrowLeftCircle onClick={handleLeftArrow}/>}
+            {pagesUi?.map((value, i) => {
+                return <p onClick={() => handleRedirect(i + 1)}
+                          className={page === i + 1 ? styles.pageChecked : styles.pageDefault} key={i}>{value}</p>
+            })
+            }
+            {page !== totalPages && <BsArrowRightCircle onClick={handleRightArrow}/>}
+        </div>
     </div>
 }
 
