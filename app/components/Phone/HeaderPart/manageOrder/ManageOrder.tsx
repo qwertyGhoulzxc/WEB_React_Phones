c6fe72c0420f51457374e921146568618f97abed
qwertyGhoulzxc <102239801+qwertyGhoulzxc@users.noreply.github.com>
2023-06-20 18:01:03 +0300

    Update ManageOrder.tsx

--- app/components/Phone/HeaderPart/manageOrder/ManageOrder.tsx
+++ app/components/Phone/HeaderPart/manageOrder/ManageOrder.tsx
@@ -22,13 +22,13 @@ const ManageOrder: FC<Props> = ({price}) => {
         setUserData(data)
     }
     return <div className={styles.box}>
-        <h2>{price.price.toLocaleString('ru-RU')} BYN</h2>
+        <h2>{price?.price.toLocaleString('ru-RU')} BYN</h2>
         {price.discountPrice &&
             <h3 className={styles.discountPrice}>{price.discountPrice.toLocaleString('ru-RU')} BYN</h3>}
         <p style={{marginBottom: '5px'}}><span>Доставка:</span> бесплатно</p>
         <p style={{marginBottom: '10px'}}><span>Самовывоз:</span> С 13:00 до 19:00,<br/>по адресу - <span
             style={{color: "#1EB53A"}}>ул. Минская 12</span></p>
-        {!user.shoppingBasket.some(obj => obj.id === phoneIId) ?
+        {!user.shoppingBasket.some(obj => obj?.id === phoneIId) ?
             <button onClick={handleAddToBasket} className={styles.addToBasket}>В корзину</button> :
             <button onClick={handleDelete} className={styles.deleteFromBasket}>убрать</button>}
 
